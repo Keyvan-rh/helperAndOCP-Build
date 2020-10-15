9af99833572edc6d9eb6c28c986a03801fd2e107
keyvan pishevar <kpisheva@redhat.com>
2020-10-15 10:04:50 -0500

    Update 00-installPackages.sh

--- 00-installPackages.sh
+++ 00-installPackages.sh
@@ -1,4 +1,3 @@
 # yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
 yum -y install ansible git
-git clone https://github.com/Keyvan-rh/helperAndOCP-Build.git
-cd helperAndOCP-Build
+
